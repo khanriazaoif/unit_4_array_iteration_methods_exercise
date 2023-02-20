ff36a6e7a734944cabe0d1afea60f18d160355c6
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-20 12:43:58 -0600

    Unit 4 JavaScript Array Iteration Methods course second section first video on remove array items with filter()v2

--- S2V1_filter/iteration.js
+++ S2V1_filter/iteration.js
@@ -30,7 +30,12 @@
 //
 // console.log(sNames);
 
-const numbers = [1,2,3,4,5];
-const no3 = numbers.filter(number => number !== 3);
+// const numbers = [1,2,3,4,5];
+// const no3 = numbers.filter(number => number !== 3);
+//
+// console.log(no3);
 
-console.log(no3);
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const evens = numbers.filter(number => number % 2 === 0);
+
+console.log(evens);
