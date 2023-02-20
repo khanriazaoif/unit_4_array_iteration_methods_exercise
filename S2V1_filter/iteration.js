4c88c85802d5c5e47107fc940782f024e7ee229f
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-20 12:40:58 -0600

    Unit 4 JavaScript Array Iteration Methods course second section first video on remove array items with filter()

--- S2V1_filter/iteration.js
+++ S2V1_filter/iteration.js
@@ -1,11 +1,36 @@
-const names = ['Selma', 'Ted', 'Mike', 'Sam', 'Sharon', 'Marvin'];
+// const names = ['Selma', 'Ted', 'Mike', 'Sam', 'Sharon', 'Marvin'];
+//
+// const sNames = names.filter(name => {
+//   if(name.charAt(0) === 'S') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+//
+//
+//
+// console.log(sNames);
 
-let sNames = [];
 
-names.forEach(name => {
-  if(name.charAt(0) === 'S') {
-    sNames.push(name);
-  }
-});
 
-console.log(sNames);
+// const names = ['Selma', 'Ted', 'Mike', 'Sam', 'Sharon', 'Marvin'];
+//
+// const sNames = names.filter(name => name.charAt(0) === 'S');
+//
+//
+//
+// console.log(sNames);
+
+// const names = ['Selma', 'Ted', 'Mike', 'Sam', 'Sharon', 'Marvin'];
+// const startsWithS = name => name.charAt(0) === 'S';
+// const sNames = names.filter(startsWithS);
+//
+//
+//
+// console.log(sNames);
+
+const numbers = [1,2,3,4,5];
+const no3 = numbers.filter(number => number !== 3);
+
+console.log(no3);
