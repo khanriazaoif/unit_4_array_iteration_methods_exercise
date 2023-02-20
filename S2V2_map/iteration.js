f935a0e0c719687f6c71d46105caaa0855ef71ea
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-20 13:08:11 -0600

    Unit 4 JavaScript Array Iteration Methods course second section first video on transform array items with map()

--- S2V2_map/iteration.js
+++ S2V2_map/iteration.js
@@ -1,1 +1,39 @@
-const strings = ['1','2','3','4','5'];
+// const strings = ['1','2','3','4','5'];
+// const numbers = strings.map(string => parseInt(string, 10));
+//
+// console.log(numbers);
+// console.log(typeof numbers);
+
+
+// const fruits = ['apple', 'pear', 'cherry'];
+//
+// let capitalizedFruits = [];
+//
+// fruits.forEach(fruit => {
+//     let capitalizedFruit = fruit.toUpperCase();
+//     capitalizedFruits.push(capitalizedFruit);
+// });
+//
+// console.log(capitalizedFruits);
+
+// const fruits = ['apple', 'pear', 'cherry'];
+//
+// let capitalizedFruits = [];
+//
+// const uppercase = fruits.map(fruit => fruit.toUpperCase());
+// capitalizedFruits.push(uppercase);
+//
+// console.log(capitalizedFruits);
+
+// const fruits = ['apple', 'pear', 'cherry'];
+//
+// const capitalizedFruits = fruits.map(fruit => fruit.toUpperCase());
+//
+// console.log(capitalizedFruits);
+
+const prices = [5, 4.23, 6.4, 8.09, 3.20];
+const priceInDollars = price => `$${price.toFixed(2)}`;
+const displayPrices = prices.map(priceInDollars
+);
+
+console.log(displayPrices);
