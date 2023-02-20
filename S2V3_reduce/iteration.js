10fbcaf838ec5d1b7075d10cd9b85986e722196a
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-20 13:30:13 -0600

    Unit 4 JavaScript Array Iteration Methods course second section last video on return a single value from an array with reduce()v3

--- S2V3_reduce/iteration.js
+++ S2V3_reduce/iteration.js
@@ -4,8 +4,19 @@
 //
 // console.log(total);
 
-const prices = [6.75, 3.10, 4.00, 8.12]; // Total: 21.97
+// const prices = [6.75, 3.10, 4.00, 8.12]; // Total: 21.97
+//
+// const total = prices.reduce((sum, price) => sum + price, 0)
+//
+// console.log(total);
 
-const total = prices.reduce((sum, price) => sum + price, 0)
+const names = ['Gary', 'Pasan', 'Gabe', 'Treasure', 'Gengis', 'Gladys', 'Tony'];
 
-console.log(total);
+const gName = names.reduce((letter, name) => {
+    if(name[0] === 'G'){
+        return letter + 1;
+    }
+    return letter;
+},0);
+
+console.log(gName);
