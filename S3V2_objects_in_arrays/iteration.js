b0627f9cf41f8434d444a93fd61c30539b3f893c
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-20 18:30:06 -0600

    Unit 4 Array iteration methods exercise section three video one. Chaining Array Methods mini quiz

--- S3V2_objects_in_arrays/iteration.js
+++ S3V2_objects_in_arrays/iteration.js
@@ -1,3 +1,20 @@
+// const years = [1989, 2015, 2000, 1999, 2013, 1973, 2012];
+// let displayYears;
+
+// displayYears should be: ["2015 A.D.", "2013 A.D.", "2012 A.D."]
+// Write your code below
+
+// displayYears = years.filter(number => number >= 2001)
+//     .map(number => number);
+// displayYears = `${displayYears} A.D.`;
+
+// displayYears = years
+//     .filter(number => number >= 2001)
+//     .map(number => `${number} A.D.`);
+//
+//
+// console.log(displayYears);
+
 const users = [
   {name: 'Samir'},
   {name: 'Angela'},
