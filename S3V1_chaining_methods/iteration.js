0bb3f387fd6f3971eed482c5c825149c2ac4d0e7
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-02-20 18:17:53 -0600

    Unit 4 Array iteration methods exercise section three video one. Chaining Array Methods

--- S3V1_chaining_methods/iteration.js
+++ S3V1_chaining_methods/iteration.js
@@ -1,6 +1,7 @@
 const arr = [1,2,3];
 
-const smallerArr = arr.filter(number => number !== 2);
-const incrementedArr = smallerArr.map(number => number + 1);
+const smallerArr = arr
+    .filter(number => number !== 2)
+    .map(number => number + 1);
 
-console.log(incrementedArr); // => [2,4]
+console.log(smallerArr); // => [2,4]
